d70e2131858a24312aba6a189f03799385dfdbfc
Aryan <aryanghugare75@gmail.com>
2026-03-05 09:31:22 +0530

    Issues

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -5,7 +5,7 @@ function WeatherDisplay({ data }) {
     <div>
       <h2>Weather Report</h2>
       <span style={{ color: data.temperature > 20 ? 'red' : 'blue' }}>
-      <p> Temperature : {data.temperature} </p> 
+      <p>Temperature: {data.temperature}</p> 
       </span>
       <p>Conditions: {data.conditions}</p>
     </div>
